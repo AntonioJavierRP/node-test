e003cd5b489a0fa98caaa89eee0ea8a3e0a99dbf
Antonio Javier Rodriguez Perez <antoniojrp1996@gmail.com>
2018-11-16 13:44:23 +0100

    changed to my application(index y test)

--- index.js
+++ index.js
@@ -1,12 +1,84 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const Joi = require('joi');
+const express = require('express');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.json());    // Para parsear el json object.
+
+var courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+]
+
+app.get('/', (req,res) => {
+    res.send('Hello World!!!!');
+});
+
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
+})
+
+app.post('/api/courses', (req,res) => {
+    const {error} = validateCourse(req.body);
+
+    if(error){
+        // 400 bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+
+// /api/courses/1
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course not found');
+    res.send(course);
+}); //:id define un parametro id (id es el nombre del parametro)
+
+// PORT
+const port = process.env.PORT || 3000;  // Environment variable PORT (set on temrminal, if it doesnt exists it uses 3000)
+app.listen(port, () => console.log(`Listening on port ${port}...`));    // use `` instead of '' so we can use template string $()
+
+app.put('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course not found');
+
+    const {error} = validateCourse(req.body);
+
+    if(error){
+        // 400 bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+ // Usamos Joi para que todas las condiciones de la estructura estén en un mismo sitio y no repetir código
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+// para poder testear
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,71 @@
+var request = require('supertest')
+    , assert = require('assert')
+    , app = require('../index.js');
+
+
+describe("infopages", function(){
+    it("shows hello world", function(done){
+        request(app).get("/")
+            .expect(200)
+            .expect(/Hello World!!!!/)
+            .end(done);
+    })
+    it("shows courses", function(done){
+        request(app).get("/api/courses")
+            .expect(200)
+            .end(done);
+    })
+})
+
+
+describe("Manage Course", function(){
+
+    var curso;
+    it("adds a new course", function(done){
+        request(app).post("/api/courses")
+            .send({ "name": "new course"})
+            .expect(200)
+            .end(done);
+    })
+
+    it("shows expecific course", function(done){
+        request(app).get("/api/courses/4")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(done);
+    });
+
+    it("modifies a specific course", function(done){
+        request(app).put("/api/courses/4")
+            .send({ "name": "changed course"})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(done);
+    });
+
+    it("deletes a specific course", function(done){
+        request(app).delete("/api/courses/4")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(done);
+    });
+    
+})
+
+
+/*
+it("deletes a specific course", function(done){
+    request(app).delete("/api/courses/4")
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(error,res){
+            console.log(curso);
+            curso = res.body;
+            console.log(curso);
+            console.log("wait a sec");
+            assert.equal(res.body, curso)
+            console.log(res.body);
+            done();
+        });
+});
+*/
